7d0aec85ffc303eb732ad9d3b0f62514ff2a4dd5
Gabriel Rozendo <79455923+rozendox@users.noreply.github.com>
2023-02-17 08:20:24 -0300

    Create soma.ts
    Nesse exemplo, criamos uma função chamada somar que recebe dois parâmetros, a e b, ambos do tipo number. Dentro da função, utilizamos o operador + para somar a e b e retornar o resultado.
    
    Em seguida, declaramos duas variáveis num1 e num2 com os valores que queremos somar e chamamos a função somar passando essas variáveis como argumentos. Armazenamos o resultado da função em uma terceira variável chamada resultado e exibimos o resultado no console usando o método console.log.

---
+++ soma.ts
@@ -0,0 +1,8 @@
+function somar(a: number, b: number): number {
+  return a + b;
+}
+
+const num1 = 10;
+const num2 = 5;
+const resultado = somar(num1, num2);
+console.log(resultado); // exibe "15" no console
